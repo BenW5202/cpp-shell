ababbdcc7491fc6d9607a0a7a09564e3c9a150f1
Ben <bwu204@gmail.com>
2026-07-25 16:20:46 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 
+// REMINDER: codecrafters submit to test code
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
+  std::cout << "$ ";
 }
