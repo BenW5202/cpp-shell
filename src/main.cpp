db94b60da282f8d8e7780b78ac604edfeac028c8
Ben <bwu204@gmail.com>
2026-07-25 17:32:05 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string input;
-  std::cin >> input;
-
-  std::cout << input << ": command not found";
+  
+  while (true){ 
+    std::cout << "$ ";
+    std::string input;
+    std::cin >> input;
+    std::cout << input << ": command not found" << std::endl;
+  }
 
 }
